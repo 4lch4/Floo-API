657356a13637abc39e19f30db79ee905f5ef5762
4lch4 <git@4lch4.email>
2022-03-29 04:22:54 -0500

    feat: added Makefile for easy tooling

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
+import { config } from 'dotenv'
+import { join } from 'path'
 import { getAppConfig, Server } from './lib'
 
 try {
+  // Attempt to import the .env file in case we're running outside of Docker.
+  config({ path: join(__dirname, '..', '.env') })
+
   const AppConfig = getAppConfig()
 
   if (AppConfig) {
